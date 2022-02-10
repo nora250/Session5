ebd4d0e48f29437ce7ae90d8c33f136a592c1d08
nora <72287273+nora250@users.noreply.github.com>
2022-02-11 00:19:10 +0900

    新規ToDO作成、未完了ToDoの書き換えまで

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputtext = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ作成
+  const li = document.createElement("li");
+  li.innerText = inputtext;
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  //console.log(div);
+
+  //未完了リストに追加
+  document.getElementById("imcomplate-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
